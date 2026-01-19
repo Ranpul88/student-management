be7b0fc75e3294cbb913a3ea3ce4bcbd18f26bc3
Ranpul88 <vithanagevinuja@gmail.com>
2026-01-19 13:12:08 +0530

    admin time table ✏️

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -4,12 +4,12 @@ export default function AdminLayout({ children }){
     return(
         <div className="w-full h-full flex bg-accent items-center justify-center">
             <div className="w-75 h-full bg-accent flex justify-center p-3">
-                <div className="w-full h-full bg-white rounded-4xl pt-2">
-                    <div className="w-full h-30 flex items-center">
-                        <img src="/logo.png" alt="" className="h-20 pl-2"/>
-                        <h2 className="text-xl font-medium text-accent">Admin</h2>
+                <div className="w-full h-full flex flex-col items-center bg-white rounded-4xl pt-4">
+                    <div className="w-40 h-30 flex items-center">
+                        <img src="/logo.png" alt="logo" className="h-25 pt-4"/>
+                        <h2 className="text-xl font-medium text-accent text-center">Admin</h2>
                     </div>
-                    <div className="w-full h-[calc(100%-120px)] flex flex-col items-center text-2xl pt-8 gap-2">
+                    <div className="w-full h-[calc(100%-120px)] flex flex-col items-center text-2xl pt-6 gap-2">
                         <Link href="/admin" className="w-30 flex items-center h-12 hover:text-accent">Students</Link>
                         <Link href="/admin/lecturers" className="w-30 flex items-center h-12 hover:text-accent">Lecturers</Link>
                         <Link href="/admin/courses" className="w-30 flex items-center h-12 hover:text-accent">Courses</Link>
